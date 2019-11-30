028148e8fad02be7a7c4ea7d313d44edcb04ba98
Preeti Sajjan <preetisajjan13@gmail.com>
2019-11-30 18:49:53 +0000

    Implemented command line input for json file

--- src/Solution_2dee498d.py
+++ src/Solution_2dee498d.py
@@ -4,57 +4,36 @@ Created on Tue Nov 26 21:49:11 2019
 
 @author: Preeti Sajjan
 """
+import os
+import sys
+import Utils.common_utilities as utils
 
-import json
-import numpy as np
-import matplotlib.pyplot as plt
+os.chdir("..")
+os.chdir("data")
+file = sys.argv[1]
+train_input,train_output,test_input,test_output = utils.json_reader(file)
 
-def json_arc_reader(file_name):
-    json_file = open(file_name)
-    data = json.load(json_file)
-    #print(data)
-    train_inputs = [data['train'][i]['input'] for i in range(len(data['train']))]
-    train_outputs = [data['train'][i]['output'] for i in range(len(data['train']))]
-    test_inputs = [data['test'][i]['input'] for i in range(len(data['test']))]
-    test_outputs = [data['test'][i]['output'] for i in range(len(data['test']))]
-    return train_inputs,train_outputs,test_inputs,test_outputs
+def solve(inputData):
+    array = inputData
+    solution = []
+    for i in range(len(array)):
+        temp_y = []
+        for j in range(len(array[i])):
+            ratio = (int)(len(array[i][j])/3)
+            temp_y = temp_y + [array[i][j][:ratio]]
+        solution = solution + [temp_y]   
+    return solution
 
-a,b,c,d = json_arc_reader('2dee498d.json')
-
-#training
-train_sol = []
-for i in range(len(a)):
-    temp_y = []
-    for j in range(len(a[i])):
-        ratio = (int)(len(a[i][j])/3)
-        temp_y = temp_y + [a[i][j][:ratio]]
-    train_sol = train_sol + [temp_y]
-
-print(train_sol)
-if(train_sol == b):
+##training
+print("\n\nTraining")
+output = solve(train_input)
+print("Output:\n", output)
+if(train_output == output):
     print("Training Data Verified!!!")
 
-#testing
-test_sol = []
-for i in range(len(c)):
-    temp_y = []
-    for j in range(len(c[i])):
-        ratio = (int)(len(c[i][j])/3)
-        temp_y = temp_y + [c[i][j][:ratio]]
-    test_sol = test_sol + [temp_y]
-
-print(test_sol)
-if(test_sol == d):
-    print("Testing Data Verified!!!")
-
-input = [a, train_sol]
-def visualize(input):
-    """
-    Function to plot grids and emulate testing interface.
-    input = A list of test input and computed output.
-    """
-    for i in range(len(input)):
-        plt.matshow(input[i])
-        plt.show()
-        
-visualize(input)
+##training
+print("\n\nTesting")
+output = solve(test_input)
+print("Output:\n", output)
+if(test_output == output):
+    print("Testing Data Verified!!!\n")
